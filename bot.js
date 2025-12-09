ae9694ccd0e0ead51cb9b0daf803e9bb5bdb4480
burak behlul <burakyabgu@gmail.com>
2025-12-09 17:31:38 +0300

    chore: Set up

---
+++ bot.js
@@ -0,0 +1,17 @@
+import "dotenv/config";
+import { Client, GatewayIntentBits } from "discord.js";
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+
+    ],
+});
+
+client.on("ready", () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.login(process.env.TOKEN);
